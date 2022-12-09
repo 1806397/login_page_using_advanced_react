8fc18c9cc30dbe80dc48e952a8b46220ef06e42b
Piyush Singh <1806397@kiit.ac.in>
2022-12-09 15:03:45 +0530

    useEffect and useReducer

---
+++ src/components/Login/Login.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+const passReducer = (state, action) => {
+  if (action.type === "USER_PASS") {
+    return {
+      value: action.val,
+      isValid: action.val.length >= 6,
+    };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.length >= 6 };
+  }
+  return { value: "", isValid: false };
+};
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
+  const [passState, dispatchPass] = useReducer(passReducer, {
+    value: "",
+    isValid: false,
+  });
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passIsValid } = passState;
+  useEffect(() => {
+    //with every keystroke useEffect is called which is not good in terms of efficiency
+    const timerForEffect = setTimeout(() => {
+      console.log("checking for validity");
+      setFormIsValid(emailState.isValid && passState.isValid);
+    }, 500);
+    //we are cleaning the timer everytime use press the button,therefore useEffect will called
+    //only when user is done entering data
+    return () => {
+      clearTimeout(timerForEffect);
+    };
+  }, [emailIsValid, passIsValid]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const passwordChangeHandler = (event) => {
+    // setEnteredPassword(event.target.value);
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    dispatchPass({ type: "USER_PASS", val: event.target.value });
+  };
+
+  const validateEmailHandler = () => {
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({
+      type: "INPUT_BLUR",
+    });
+  };
+
+  const validatePasswordHandler = () => {
+    // setPasswordIsValid(passState.value.trim().length > 6);
+    dispatchPass({
+      type: "INPUT_BLUR",
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, passState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button
+            type="submit"
+            className={classes.btn}
+            disabled={!(emailState.isValid && passState.isValid)}
+          >
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
